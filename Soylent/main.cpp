be4fc608a610567e770813b8ac86515a9f9f1dc0
Max He <maxhanchen@gmail.com>
2018-07-26 18:27:08 -0400

    Soylent Solution

--- Soylent/main.cpp
+++ Soylent/main.cpp
@@ -2,19 +2,24 @@
 
 using namespace std;
 
-int main() {
-  int x, y, z;
-  cin >> x;
-  
- 
-    for(int i= 0; i<x; i++){
-        cin>> y;
-    if(y%400>0){
-     z=(y/400)+1;}
-    else{
-        z=y/400;}
-     
-     cout << z << endl;
+int main()
+{
+    int x, y, z;
+    cin >> x;
+
+    for (int i = 0; i < x; i++)
+    {
+        cin >> y;
+        if (y % 400 > 0)
+        {
+            z = (y / 400) + 1;
+        }
+        else
+        {
+            z = y / 400;
+        }
+
+        cout << z << endl;
     }
-        return 0;
+    return 0;
 }
